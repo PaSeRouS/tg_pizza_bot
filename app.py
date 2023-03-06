4fb1ab7ca37b650e71bdc85f15bc19e698ea5b25
PaSeRouS <paser96@mail.ru>
2023-03-06 03:26:53 +0300

    Отправка меню пиццерии в бота при любом сообщении

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import requests
 from flask import Flask, request
 
+from moltin_api import get_products
+
 app = Flask(__name__)
 FACEBOOK_TOKEN = os.environ["PAGE_ACCESS_TOKEN"]
 
@@ -25,14 +27,14 @@ def webhook():
     Основной вебхук, на который будут приходить сообщения от Facebook.
     """
     data = request.get_json()
+    # print(data)
     if data["object"] == "page":
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 if messaging_event.get("message"):
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
-                    message_text = messaging_event["message"]["text"]
-                    send_message(sender_id, message_text)
+                    send_menu(sender_id)
     return "ok", 200
 
 
@@ -52,6 +54,73 @@ def send_message(recipient_id, message_text):
         params=params, headers=headers, json=request_content
     )
     response.raise_for_status()
+
+
+def send_menu(recipient_id):
+    params = {"access_token": FACEBOOK_TOKEN}
+    headers = {"Content-Type": "application/json"}
+
+    request_content = {
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": get_elements_for_generic()
+                }
+            }
+        }
+    }
+
+    response = requests.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params=params, headers=headers, json=request_content
+    )
+    response.raise_for_status()
+
+
+def get_elements_for_generic():
+    # В сообщении кнопки выводятся по три штуки в сообщении и только на 10 слайдах
+    elements = []
+    buttons = []
+    index = 0
+
+    products = get_products(os.environ["CLIENT_ID"], os.environ["CLIENT_SECRET"])
+
+    for product_name, product_id in products.items():
+        if index % 3 == 0 and index != 0 and index < 30:
+            element = {
+                "title": "Меню пиццерии",
+                "buttons": buttons
+            }
+
+            elements.append(element)
+
+            buttons = []
+
+        index += 1
+
+        button = {
+            "type": "postback",
+            "title": product_name,
+            "payload": product_id,
+        }
+
+        buttons.append(button)
+
+    if index < 30:
+        element = {
+            "title": "Меню пиццерии",
+            "buttons": buttons
+        }
+
+        elements.append(element)
+
+    return elements
+
 
 if __name__ == '__main__':
     app.run(debug=True)
